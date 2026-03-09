7cca06f4677f7ac754ea85709491ccc38f6f3578
leastzero <csong7177@naver.com>
2026-03-09 20:56:54 +0900

    [feat: Post Upload Modal UI 구현]

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ProfilePage from './pages/Profile/Profile';
 import { Route, Routes } from 'react-router-dom';
 import PhotoUpload from './pages/PhotoUpload/PhotoUpload';
+import Home from './pages/Home/Home';
 
 const App: React.FC = () => {
   return (
     <>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/profile' element={<ProfilePage />} />
         <Route path='/upload' element={<PhotoUpload />} />
       </Routes>
