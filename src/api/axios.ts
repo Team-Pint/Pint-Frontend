bafd37de499123f1a54f1a70cb5d948495e70ecc
harukiki97 <qwejinoohan@gmail.com>
2026-03-12 17:34:54 +0900

    [fix: axios 파일 수정]

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://3.36.117.181:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
